3f4f7b35235fc23f082305daa118dfbfee90a547
aaronferr <114663986+AaronTheWorld@users.noreply.github.com>
2023-01-19 22:36:11 +0100

    Add files via upload

---
+++ tarefa5_ducias2ovos/scriptito.js
@@ -0,0 +1,9 @@
+var ducias;
+ducias= prompt("¿Cantas ducias ten a megatortilla? ");
+var ducia= parseInt(ducias);
+alert("Asi que fan falta "+ducia+" ducias.");
+var huevos= ducias*12;
+alert("Iso son "+huevos+" ovos.");
+document.write("<hr>")
+document.write("Ducias: "+ducias+"<hr>")
+document.write("Ovos: "+huevos+"<hr>")
